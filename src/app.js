18441bd7480773d2ddf567e92e250dbb18308c4f
danielmoret <dmoret17@gmail.com>
2022-09-02 02:08:33 +0000

    second commit

--- src/app.js
+++ src/app.js
@@ -8,24 +8,23 @@ import "./assets/img/4geeks.ico";
 window.onload = () => {
   //write your code here
   document.querySelector("#btn-excuse").addEventListener("click", () => {
-    document.querySelector("#the-excuse").innerHTML = generateExcuse();
+    document.querySelector("#excuse").innerHTML = excuseGenerator();
   });
 };
 
-let generateExcuse = () => {
-  let who = ["the dog", "my granma", "his turtle", "my bird"];
-  let what = ["eat", "pissed", "crushed", "broked"];
-  let when = [
-    "before the class",
-    "right in time",
-    "when I finished",
-    "during my lunch",
-    "while I was praying"
-  ];
+let who = ["the dog", "my granma", "his turtle", "my bird"];
+let what = ["eat", "pissed", "crushed", "broked"];
+let when = [
+  "before the class",
+  "right in time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
 
-  let whoIndex = Math.floor(Math.random() * who.length);
-  let whatIndex = Math.floor(Math.random() * what.length);
-  let whenIndex = Math.floor(Math.random() * when.length);
+const randomNumber = array => Math.floor(Math.random() * array.length);
 
-  return `${who[whoIndex]} ${what[whatIndex]} ${when[whenIndex]}`;
-};
+const excuseGenerator = () =>
+  `${who[randomNumber(who)]} ${what[randomNumber(what)]} ${
+    when[randomNumber(when)]
+  }`;
